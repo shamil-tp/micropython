ce036669ab31cae926e5aa2528077c23defb146c
shamil <for.photos.only001@gmail.com>
2026-06-05 00:49:32 +0530

    add config.py for ESP32 alarm project

---
+++ ESP32/alarm/config.py
@@ -0,0 +1,16 @@
+# config.py
+
+WIFI_SSID = "THOUTTUNGAL4G"
+WIFI_PASSWORD = "shamil927"
+
+PINS = {
+    "I2C_SDA": 21,
+    "I2C_SCL": 22,
+    "LED_GREEN": 18,
+    "LED_YELLOW": 19,
+    "LED_RED": 23,
+    "BUZZER": 2
+}
+
+I2C_ADDR_LCD = 0x27
+I2C_ADDR_RTC = 0x68
